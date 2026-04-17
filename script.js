0aacaf85c36b821987958ccce97b165aa0bd859a
muraoka2016 <felipemuraoka2016@outlook.com>
2026-04-17 18:21:24 -0300

    finished implementing logic for base game

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 let container = document.querySelector(".container");
-container.style.height = "100vh";
-container.style.display = "flex";
-container.style.flexDirection = "column";
+let inputNumber = document.querySelector("#inputNumber");
+let change = document.querySelector("#change");
+let warning = document.querySelector("#warning");
 
-const size = 16;
+let draw = false;
+document.addEventListener("mousedown", () => {
+  draw = true;
+});
 
-for (let i = 0; i < size; i++) {
-  const line1 = document.createElement("div");
-  line1.style.display = "flex";
-  line1.style.flex = "1";
-  container.appendChild(line1);
+document.addEventListener("mouseup", () => {
+  draw = false;
+});
 
-  for (let j = 0; j < size; j++) {
-    const square = document.createElement("div");
-    square.style.flex = "1";
-    square.style.border = "1px solid black";
-    square.style.aspectRatio = "1 / 1";
+let size = 16;
+const squareSize = 100 / size;
 
-    line1.appendChild(square);
+change.addEventListener("click", () => {
+  if (
+    inputNumber.value <= 0 ||
+    inputNumber.value > 100 ||
+    inputNumber.value === ""
+  ) {
+    return (warning.textContent =
+      "Invalid entry! The entry should be:\n1) Positive\n2) Max value 100\n3) Can't be empty");
+  } else {
+    container.textContent = "";
+    size = inputNumber.value;
+    createGrid();
+  }
+});
+
+function createGrid() {
+  for (let i = 0; i < size; i++) {
+    const line1 = document.createElement("div");
+    line1.style.display = "flex";
+    line1.style.flex = "1";
+    container.appendChild(line1);
+
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = `${squareSize}%`;
+      square.style.flex = "1";
+      square.style.boxSizing = "border-box";
+      square.style.border = "1px solid black";
+      square.style.aspectRatio = "1 / 1";
+
+      square.addEventListener("mousemove", () => {
+        if (draw) {
+          square.style.backgroundColor = "black";
+        }
+      });
+
+      line1.appendChild(square);
+    }
   }
 }
+
+createGrid();
